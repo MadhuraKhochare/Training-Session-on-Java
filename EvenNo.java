b3dbb2fd67d416749d56ba706e0f9d4c9fb2cdd8
MadhuraKhochare <kmadhura2209@gmail.com>
2024-06-11 15:15:09 +0530

    this file irs related to evenno

---
+++ EvenNo.java
@@ -0,0 +1,16 @@
+package MyPack2;
+
+public class EvenNo {
+
+	public static void main(String args[])
+	{
+		System.out.println("Even Numbers are:");
+		for(int i=10;i>0;i--)
+		{
+			if(i % 2==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+}
